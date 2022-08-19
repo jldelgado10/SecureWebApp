75a9ea6118469bf2c8ffe3e05508a46a2ac0457c
Jose Luis Delgado <joseluis.delgado@nordstrom.com>
2022-08-18 20:57:56 -0500

    Identity tested.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SecureWebApp.Models;
 using System;
@@ -23,6 +24,7 @@ namespace SecureWebApp.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
